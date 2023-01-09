5f8a211cde2024c5e3debebf5a2112dc696f0508
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2023-01-09 15:22:49 +0000

    Cluers can see what color they are clueing

--- public_files/colorwheel.js
+++ public_files/colorwheel.js
@@ -1,17 +1,17 @@
 function coordsToAngle(x, y) { return Math.atan2(y, x) * 180 / Math.PI; }
 Math.dist = (p1, p2) => Math.sqrt(((p2[0] - p1[0]) ** 2) + ((p2[1] - p1[1]) ** 2))
-function getColorWheelPointColor(rect, x, y) {
-	var ypc = (y - rect.top) / rect.height
-	var xpc = (x - rect.left) / rect.width
+function getColorWheelPointColor(x, y) {
+	var ypc = y / 480
+	var xpc = x / 800
 	xpc = (xpc - 0.5) * 2
 	ypc = (ypc - 0.5) * 2
 	var angle = coordsToAngle(xpc, ypc) + 90
 	if (angle < 0) angle += 360
 	// Get the value
 	var val = 50
-	var dist = Math.dist([rect.left + (rect.width / 2), rect.top + (rect.height / 2)], [x - rect.left, y - rect.top])
-	if (dist < rect.width / 4) {
-		var pc = dist / (rect.width / 3)
+	var dist = Math.dist([400, 240], [x, y])
+	if (dist < 200) {
+		var pc = dist / 266
 		val += (1 - pc) * 50
 	}
 	return `hsl(${angle}deg, 100%, ${val}%)`
